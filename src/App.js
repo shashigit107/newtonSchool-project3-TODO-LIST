8a752cedd0eb418b184f65732f22a94c37aa232e
shashigit107 <shashiranjan107@gmail.com>
2021-07-12 23:51:45 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import react from "react";
 import Compt1 from "./Components/Compt1";
 function App() {
   return (
-        <>
+        <div>
           <Compt1/>
-        </>
+        </div>
     
   );
 }
